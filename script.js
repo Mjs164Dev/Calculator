c2e35314418a6bcf750b63592c4a5658013d4bed
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-01-18 21:50:07 -0500

    Make code more readable.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let displayText = "";
 
-// Add a common class to all your buttons, e.g., 'calc-button'
 mainContainer.addEventListener("click", function(event) {
-    // Check if the clicked element has the 'calc-button' class
+    // Check if the clicked element has the 'num-button' class
     if(event.target.classList.contains('number-button')) {
-        const value = event.target.textContent; // or get some attribute that holds the value
-        displayText += value;
+        const btnValue = event.target.textContent;
+        displayText += btnValue;
         resultsContainer.textContent = displayText;
     }
 });
