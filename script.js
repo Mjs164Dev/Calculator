4e62afa906b7f92636d1d1e91123c8fc323732d1
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-01-18 20:57:26 -0500

    Update result container with number btn click.

--- script.js
+++ script.js
@@ -13,3 +13,53 @@ const btnAdd = document.querySelector('#button-add');
 const btnSubtract = document.querySelector('#button-subtract');
 const btnMultiply = document.querySelector('#button-multiply');
 const btnDivide = document.querySelector('#button-divide');
+const btnAC = document.querySelector('#button-ac');
+
+btnOne.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+    resultsContainer.textContent = 1;
+});
+
+btnTwo.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+    resultsContainer.textContent = 2;
+});
+
+btnThree.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+    resultsContainer.textContent = 3;
+});
+
+btnFour.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+    resultsContainer.textContent = 4;
+});
+
+btnFive.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+    resultsContainer.textContent = 5;
+});
+
+btnSix.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+    resultsContainer.textContent = 6;
+});
+
+btnSeven.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+    resultsContainer.textContent = 7;
+});
+
+btnEight.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+    resultsContainer.textContent = 8;
+});
+
+btnNine.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+    resultsContainer.textContent = 9;
+});
+
+btnAC.addEventListener("click", function() {
+    resultsContainer.textContent = '';
+});
