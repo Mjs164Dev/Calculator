21d7307c792a64bc9299f88cce4dabb0257a3662
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-01-18 21:43:11 -0500

    clean up more unused code.

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const mainContainer = document.querySelector('#main-container');
-const resultsContainer = document.querySelector('#results-container');
-const btnZero = document.querySelector('#button-zero');
-const btnOne = document.querySelector('#button-one');
-const btnTwo = document.querySelector('#button-two');
-const btnThree = document.querySelector('#button-three');
-const btnFour = document.querySelector('#button-four');
-const btnFive = document.querySelector('#button-five');
-const btnSix = document.querySelector('#button-six');
-const btnSeven = document.querySelector('#button-seven');
-const btnEight = document.querySelector('#button-eight');
-const btnNine = document.querySelector('#button-nine');
-const btnAdd = document.querySelector('#button-add');
-const btnSubtract = document.querySelector('#button-subtract');
-const btnMultiply = document.querySelector('#button-multiply');
-const btnDivide = document.querySelector('#button-divide');
-const btnAC = document.querySelector('#button-ac');
-const btnDecimal = document.querySelector('#button-decimal');
-const btnEqual = document.querySelector('#button-equal');
-
 let displayText = "";
 
 // Add a common class to all your buttons, e.g., 'calc-button'
