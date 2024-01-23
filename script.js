3a755898c865f22d23c55a28688325407edda224
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-01-23 17:12:07 -0500

    outlined new computing logic

--- script.js
+++ script.js
@@ -6,34 +6,16 @@ let firstNumber = "";
 let operator = "";
 let secondNumber = "";
 let answer = "";
-let enteredValue = "";
-let numberList = [];
 
-// mainContainer.addEventListener("click", function(event) {
-//     // Check if the clicked element has the 'num-button' class
-//     if(event.target.classList.contains('number-button')) {   
-//         // Check to see if an operator has been selected
-//         if(operator === "") {
-//             const btnValue = event.target.textContent;
-//             firstNumber += btnValue;
-//             resultsContainer.textContent = firstNumber;
-//         }
-//         else {
-//             const btnValue = event.target.textContent;
-//             secondNumber += btnValue;
-//             resultsContainer.textContent = secondNumber;
-//         }
-//     }
-//     else if (event.target.classList.contains('operator-button')) {
-//         const btnValue = event.target.textContent;
-//         operator = btnValue;
-//         displayText = "";
-//         resultsContainer.textContent = displayText;
-//     }
-// });
+//Check values of firstNumber (F), operator (O), and secondNumber (S)
+//if all are blank -> entering firstNumber
+//if F has value -> still entering first number
+//if F & O have value, entering second number
+//if all have value, call compute function and set answer to first number, then reset O & S values
+
 
 mainContainer.addEventListener("click", function(event) {
-    // Check if the clicked element has the 'num-button' class
+    // Check if the clicked element has the 'number-button' class
     if(event.target.classList.contains('number-button')) {   
         // Check to see if an operator has been selected
         if(operator === "") {
@@ -55,24 +37,21 @@ mainContainer.addEventListener("click", function(event) {
     }
 });
 
+
 // Use firstNumber, secondNumber, and the operator to calculate the answer
 function operate(firstNumber, secondNumber, operator) {
     switch (operator) {
         case "+":
             answer = Number(firstNumber) + Number(secondNumber);
-            console.log('addition!');
             break;
         case "-":
             answer = Number(firstNumber) - Number(secondNumber);
-            console.log('subtraction!');
             break;
         case "*":
             answer = Number(firstNumber) * Number(secondNumber);
-            console.log('multiplication!');
             break;
         case "/":
             answer = Number(firstNumber) / Number(secondNumber);
-            console.log('division!');
             break;
     }
     resultsContainer.textContent = answer;
@@ -93,6 +72,7 @@ document.querySelector('#button-equal').addEventListener("click", function() {
     console.log(operator);
     console.log(secondNumber);
     operate(firstNumber, secondNumber, operator);
+    console.log(answer);
     firstNumber = answer;
     operator = "";
     secondNumber = "";
