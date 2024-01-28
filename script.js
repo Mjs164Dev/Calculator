a173625cef76e9e59ae1c62121d2f20af9441f98
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-01-28 10:22:15 -0500

    add comments and remove console.log statements

--- script.js
+++ script.js
@@ -1,58 +1,60 @@
+// set mainContainer & buttoContainer variables
 mainContainer = document.querySelector('#main-container');
 resultsContainer = document.querySelector('#results-container');
 
+// set initial variable values
 let displayText = "";
 let firstNumber = "";
 let operator = "";
 let secondNumber = "";
 let answer = "";
 
-
-//if number button is being selected, either firstNumber or secondNumber is being entered
-//  firstNumber is being entered if operator == "", else secondNumber is being entered
-//if operator button is being selected and answer != "", firstNumber = answer.
-
-mainContainer.addEventListener("click", function(event) {
+// Set event listener for button click in main container
+mainContainer.addEventListener("click", function(event) { // pass event details to function for reference
     // Check if the clicked element has the 'number-button' class
     if(event.target.classList.contains('number-button')) {   
         // Check to see if an operator has been selected
         if(operator === "") {
-            const btnValue = event.target.textContent;
-            firstNumber += btnValue;
-            resultsContainer.textContent = firstNumber;
-            console.log("firstNumber: ", firstNumber);
+            const btnValue = event.target.textContent; // set btnValue to text value of button clicked
+            firstNumber += btnValue; // combine numbers together
+            resultsContainer.textContent = firstNumber; // display numbers selected
+            
+            // if decimal was clicked, deactivate it so it can't be selected again
             if (btnValue == ".") {
                 document.querySelector('#button-decimal').disabled = true;
             }
         }
         else {
-            const btnValue = event.target.textContent;
-            secondNumber += btnValue;
-            resultsContainer.textContent = secondNumber;
-            console.log("secondNumber: ", secondNumber);
+            const btnValue = event.target.textContent; // set btnValue to text value of button
+            secondNumber += btnValue; // combine numbers together
+            resultsContainer.textContent = secondNumber; // display numbers selected
+            
+            // if decimal was clicked, deactivate it so it can't be selected again
             if (btnValue == ".") {
                 document.querySelector('#button-decimal').disabled = true;
             }
         }
     }
+    // check if button selected was an operator button
     else if (event.target.classList.contains('operator-button')) {
         
+        // do if firstNumber, operator, and secondNumber don't have value
         if (firstNumber.length != 0 && secondNumber.length != 0 && operator.length != 0) {
-            operate(firstNumber,secondNumber,operator);
-            console.log(answer);
+            operate(firstNumber,secondNumber,operator); // call operate function
             firstNumber = answer;
-            secondNumber = "";
+            secondNumber = ""; // clear secondNumber value
             const btnValue = event.target.textContent;
             operator = btnValue;
-            console.log("operator: ", operator);
         }
+        // do if firstNumber, operator, or secondNumber have value
         else {
             const btnValue = event.target.textContent;
             operator = btnValue;
-            console.log("operator: ", operator);
-            displayText = "";
+            displayText = ""; // clear display text
             resultsContainer.textContent = displayText;
         }
+        
+        // Ensure decimal button is active after operator button is selected
         document.querySelector('#button-decimal').disabled = false;
     }
 });
@@ -60,6 +62,7 @@ mainContainer.addEventListener("click", function(event) {
 // Use firstNumber, secondNumber, and the operator to calculate the answer
 function operate(firstNumber, secondNumber, operator) {
 
+    // check if user is trying to divide by 0
     if (secondNumber == 0 && operator == "/") {
         alert("Can't divide by 0! Start over.");
         clearVariables();
@@ -79,7 +82,7 @@ function operate(firstNumber, secondNumber, operator) {
                 answer = Number(firstNumber) / Number(secondNumber);
                 break;
         }
-        resultsContainer.textContent = answer;
+        resultsContainer.textContent = answer; // display answer
     }
 };
 
@@ -90,7 +93,7 @@ function clearVariables() {
     secondNumber = "";
     answer = "";
     resultsContainer.textContent = displayText;
-    document.querySelector('#button-decimal').disabled = false;
+    document.querySelector('#button-decimal').disabled = false; // Ensure decimal button is active after all variables are cleared
 }
 
 // Reset the display text on AC button click
@@ -101,6 +104,4 @@ document.querySelector('#button-ac').addEventListener("click", function() {
 document.querySelector('#button-equal').addEventListener("click", function() {
     operate(firstNumber, secondNumber, operator);
     document.querySelector('#button-decimal').disabled = false;
-    console.log("answer: ", answer);
-    console.log(firstNumber, operator, secondNumber, "=",answer);
 });
